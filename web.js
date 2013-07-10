6e52723b766db7d3e574c38894e2e377bad5dee1
John Smith <example@stanford.edu>
2013-07-10 05:52:35 +0000

    work work work

--- web.js
+++ web.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-    fs.readFile('./index.html', function(error, content) {
+
+fs.readFile('./index.html', function(error, content) {
+    if (error) {
+	response.writeHead(500);
+	response.end();
+    }
+    else {
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.end(content, 'utf-8');
+    }
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
