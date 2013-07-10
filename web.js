6d12f6c5c8b92c05bd81bdf1eb1f0d43cc418085
John Smith <example@stanford.edu>
2013-07-10 16:02:18 +0000

    Committed index.html and web.js.

--- web.js
+++ web.js
@@ -6,9 +6,7 @@ var fs = require('fs');
 // fs.readFile('./index.html', function(error, content) {
 // });
 
-app.get('/hello.txt', function(req, res){
-    res.send('Hello World');
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
