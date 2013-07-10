23b9b0b8cc726fd180adb5aa8e6d60113d0f4da2
John Smith <example@stanford.edu>
2013-07-10 15:44:23 +0000

    Editing part of the fs.readFile file.

--- web.js
+++ web.js
@@ -4,14 +4,6 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 fs.readFile('index.html', function(error, content) {
-    if (error) {
-	response.writeHead(500);
-	response.end();
-    }
-    else {
-	response.writeHead(200, { 'Content-Type': 'text/html' });
-	response.end(content, 'utf-8');
-    }
 });
 
 
