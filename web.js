20571531ad42d0c8f7e80149a60eab9606c3f57a
John Smith <example@stanford.edu>
2013-07-10 15:57:34 +0000

    Commented out some stuff.

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
-fs.readFile('./index.html', function(error, content) {
-});
+// fs.readFile('./index.html', function(error, content) {
+// });
 
 app.get('/hello.txt', function(req, res){
     res.send('Hello World');
