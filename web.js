c02cb1709d8cec122f8c2dae8e628ec1333aafb7
John Smith <example@stanford.edu>
2013-07-10 15:55:22 +0000

    Added code after fs.readFile.

--- web.js
+++ web.js
@@ -6,6 +6,9 @@ var fs = require('fs');
 fs.readFile('./index.html', function(error, content) {
 });
 
+app.get('/hello.txt', function(req, res){
+    res.send('Hello World');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
