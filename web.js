146249e7e5d2e2529140e08dcbbcdd31c69d0659
John Smith <example@stanford.edu>
2013-08-13 04:45:51 +0000

    adsf

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fs = require('fs');
+// var fs = require('fs');
 
 // fs.readFile('./index.html', function(error, content) {
 // });
